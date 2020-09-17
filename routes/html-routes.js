a63058a232946de87f1b3373bcbefd8671fbeda6
Komplexnupe <leonard.love87@icloud.com>
2020-09-17 19:54:47 -0400

    updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,9 +10,15 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/viewEvents");
     }
+    res.sendFile(path.join(__dirname, "../public/welcome.html"));
+  });
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/viewEvents");
+    }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
