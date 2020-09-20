0a5410e8d33166ac72ee9426703b02738456ae30
Komplexnupe <leonard.love87@icloud.com>
2020-09-20 15:46:42 -0400

    routes, view, and modal updates

--- public/js/viewEvents.js
+++ public/js/viewEvents.js
@@ -7,47 +7,61 @@ $(document).ready(() => {
     $(".member-name").text(data.email);
   });
   const activityCategory = $(".activity");
-  activityCategory.on("click", function() {
+  activityCategory.on("click", function () {
     $("#linkUpModal").text(event.currentTarget.value);
-    console.log(event.currentTarget.id);
+    // console.log(event.currentTarget.id);
     $("#ol").empty();
-    $.get("/api/linkup/category/" + event.currentTarget.id, function(data) {
-      console.log("LinkUps---->", data);
+    $.get("/api/linkup/category/" + event.currentTarget.id, function (data) {
+      // console.log("LinkUps---->", data);
       displayLinkUp(data);
+      const interestBtn = $("#interest");
+      interestBtn.on("click", function () {
+        console.log("hey");
+      });
     });
   });
   function displayLinkUp(data) {
-    console.log("for loop ====>", data);
+    // console.log("for loop ====>", data);
     for (let i = 0; i < data.length; i++) {
-<<<<<<< HEAD
-      const displayEach = `${data[i].name} 
-      Where: ${data[i].street} ${data[i].city}, ${data[i].state} ${data[i].zipCode}
-      When: Date Goes Here`;
-=======
+      // console.log(convertTime(data[i].startTime));
+      const eventTime = convertTime(data[i].startTime);
       const displayEach = `
       <p id="evite" class="card-title" style="font-size: 20px">
         <b>${data[i].name}</b>
-        <button type="button" class="btn btn-primary ml-5">
+        <button type="button" id="interest" class="btn btn-primary ml-5">
                         Interested
                         <span class="badge badge-light ml-2">${data[i].interestCount}</span>
                       </button></p>
                     <p><b>Where: </b>${data[i].street} ${data[i].city}, ${data[i].state} ${data[i].zipCode}
-                    <p><b>When: </b>${data[i].linkUpDate} at ${data[i].startTime}</p>
+                    <p><b>When: </b>${data[i].linkUpDate} at ${eventTime}</p>
                     <p class="card-text"><b>What: </b>${data[i].linkUpDesc}.
                     </p>
                     <hr>
                     `;
->>>>>>> acb6ed54d88ddc8dc4cef07b45012953b8f9ad2e
-      console.log(displayEach);
+      // console.log(displayEach);
       $("#ol").append(displayEach);
     }
   }
+  function convertTime(x) {
+    let time = x; // your input
+    time = time.split(":"); // convert to array
+    // fetch
+    const hours = Number(time[0]);
+    const minutes = Number(time[1]);
+    const seconds = Number(time[2]);
+    // calculate
+    let timeValue;
+    if (hours > 0 && hours <= 12) {
+      timeValue = "" + hours;
+    } else if (hours > 12) {
+      timeValue = "" + (hours - 12);
+    } else if (hours === 0) {
+      timeValue = "12";
+    }
+    timeValue += minutes < 10 ? ":0" + minutes : ":" + minutes; // get minutes
+    timeValue += hours >= 12 ? " P.M." : " A.M."; // get AM/PM
+    // show
+    // console.log(timeValue);
+    return timeValue;
+  }
 });
-/*{ <p id="evite" class="card-title" style="font-size: 20px"><b>Glow-ga in the Park</b><button type="button"
-                    class="btn btn-primary ml-5">
-                    Interested<span class="badge badge-light ml-2">4</span>
-                  </button></p>
-                <p><b>Where: </b>Freedom Park: 1908 East Blvd. Charlotte, NC 28203
-                <p><b>When: </b>Thursday, September 24th, 2020 at 7pm </p>
-                <p class="card-text"><b>What: </b>Glow in the dark yoga in the park.
-                </p> }*/
